4a98bde802f1740ba390be71e5dffbb3b5cf0613
victor alvarez <victorfelipealvarezrojas@hotmail.com>
2023-10-24 11:22:57 -0300

    add project base ms-cursos template to k8s main

---
+++ ms-cursos/src/main/java/com/ms/cursos/MsCursosApplication.java
@@ -0,0 +1,13 @@
+package com.ms.cursos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCursosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCursosApplication.class, args);
+	}
+
+}
--- ms-usuarios/src/main/java/com/springcoud/ms/usuarios/service/UsuarioServiceInterface.java
+++ ms-usuarios/src/main/java/com/springcoud/ms/usuarios/service/UsuarioServiceInterface.java
@@ -1,10 +1,7 @@
 package com.springcoud.ms.usuarios.service;
 
-import com.springcoud.ms.usuarios.model.Usuario;
 import com.springcoud.ms.usuarios.payload.response.UsuarioResponse;
 import com.springcoud.ms.usuarios.payload.dto.UsuarioDto;
-
-import java.util.Optional;
 
 public interface UsuarioServiceInterface {
     UsuarioResponse findAll(int page, int size, String sortBy);
